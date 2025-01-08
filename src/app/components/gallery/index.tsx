075dc8219bf77502eb094c5726e3177e40eb5bcb
First-project <mehedihasan0937580@gmail.com>
2025-01-08 23:56:59 +0600

    image filtering done

--- src/app/components/gallery/index.tsx
+++ src/app/components/gallery/index.tsx
@@ -1,10 +1,18 @@
+"use client";
 import React from "react";
 import ImageCard from "./card";
 import { data } from "@/lib/db";
+import { useSearchParams } from "next/navigation";
 const IndexGallery = () => {
+  const params = useSearchParams();
+  const category = params.get("category");
+  console.log(category);
+  const image = data.filter((item) =>
+    category ? item.category === category : item
+  );
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 gap-3 justify-items-center w-full h-fit shadow-lg shadow-white bg-white py-4 px-6 rounded-md ">
-      {data.map((item) => (
+      {image.map((item) => (
         <ImageCard key={item.id} {...item} />
       ))}
     </div>
