9b2b8f83a5e674c0b8f78e4a7d34a4364467c847
First-project <mehedihasan0937580@gmail.com>
2025-01-10 00:17:15 +0600

    add image functionality done

--- src/app/components/gallery/index.tsx
+++ src/app/components/gallery/index.tsx
@@ -1,24 +1,56 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImageCard from "./card";
 import { data } from "@/lib/db";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import UploadPopup from "./uploadPopup";
+import { IImage, TCategory } from "@/types";
 const IndexGallery = () => {
+  const [images, setImages] = useState<IImage[]>([]);
   const params = useSearchParams();
   const category = params.get("category");
   const search = params.get("search");
   const upload = params.get("popup") ? true : false;
+  const route = useRouter();
+  useEffect(() => {
+    const filterData = data.filter(
+      (item) =>
+        (search
+          ? item.name.toLowerCase().includes(search.toLowerCase())
+          : item) && (category ? item.category === category : item)
+    );
+    setImages(filterData);
+  }, [category, search]);
 
-  const images = data.filter(
-    (item) =>
-      (search
-        ? item.name.toLowerCase().includes(search.toLowerCase())
-        : item) && (category ? item.category === category : item)
-  );
+  const close = () => {
+    const body = document.querySelector("body");
+    route.push("/");
+    if (body) {
+      body.className = "overflow-scroll";
+    }
+  };
+
+  const addNewItem = (name: string, category: TCategory, file: File) => {
+    const render = new FileReader();
+    render.onload = (e) => {
+      const imageId = images.length.toString();
+      const newImage = {
+        id: imageId,
+        name,
+        category,
+        url: e.target?.result as any,
+        likes: 35,
+        shares: 1,
+      };
+      setImages((prev) => [...prev, newImage]);
+      close();
+    };
+    render.readAsDataURL(file);
+  };
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 gap-3 justify-items-center w-full h-fit shadow-md shadow-white bg-white py-4 px-6 rounded-md">
-      {upload ? <UploadPopup /> : null}
+      {upload && <UploadPopup submit={addNewItem} close={close} />}
       {images.length > 0 ? (
         images.map((item) => <ImageCard key={item.id} {...item} />)
       ) : (
--- src/app/components/gallery/uploadPopup.tsx
+++ src/app/components/gallery/uploadPopup.tsx
@@ -1,16 +1,21 @@
 "use client";
-import { useRouter } from "next/navigation";
-import React from "react";
-
-const UploadPopup = () => {
-  const route = useRouter();
-  const close = () => {
-    const body = document.querySelector("body");
-    route.push("/");
-    if (body) {
-      body.className = "overflow-scroll";
-    }
+import { TCategory } from "@/types";
+import React, { FC, useState } from "react";
+interface Props {
+  submit: (name: string, category: TCategory, file: File) => void;
+  close: () => void;
+}
+const UploadPopup: FC<Props> = ({ submit, close }) => {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("photo");
+  const [file, setFile] = useState<File | null>(null);
+  const handleSubmit = () => {
+    if (name && category && file) submit(name, category as TCategory, file);
+    setName("");
+    setCategory("photo");
+    setFile(null);
   };
+
   return (
     <div className="w-full max-h-screen flex justify-center items-center absolute top-0 left-0 bg-transparent z-50">
       <div className="w-full h-screen flex justify-center items-center absolute top-0 left-0 bg-slate-950/50 z-50">
@@ -20,20 +25,26 @@ const UploadPopup = () => {
               <input
                 placeholder="Name..."
                 className="border border-slate-400 rounded-md py-2 px-4 w-full"
+                onChange={(e) => setName(e.target.value)}
               />
               <input
                 type="file"
                 className="border border-slate-400 rounded-md w-full"
+                onChange={(e) => setFile(e.target.files?.[0] || null)}
               />
               <select
                 name="category"
                 id="category"
                 className="w-full border border-slate-400 rounded-md py-2 px-4"
+                onChange={(e) => setCategory(e.target.value)}
               >
                 <option value="photo">photo</option>
                 <option value="vector">vector</option>
               </select>
-              <button className="bg-sky-400 px-4 py-2 w-full rounded-md active:translate-y-0.5 transition-all duration-200">
+              <button
+                className="bg-sky-400 px-4 py-2 w-full rounded-md active:translate-y-0.5 transition-all duration-200"
+                onClick={handleSubmit}
+              >
                 Upload
               </button>
             </div>
