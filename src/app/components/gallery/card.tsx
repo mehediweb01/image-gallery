7e2e3ff9f05ac2d2992d0496028f09a7569087e2
First-project <mehedihasan0937580@gmail.com>
2025-01-13 23:03:55 +0600

    update like option added done

--- src/app/components/gallery/card.tsx
+++ src/app/components/gallery/card.tsx
@@ -1,7 +1,16 @@
+"use client";
 import Image from "next/image";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { IImage } from "@/types";
+import saveIcon from "@/app/img/save.svg";
+import saveDone from "@/app/img/save-done.svg";
 const ImageCard: FC<IImage> = ({ likes, name, shares, url }) => {
+  const [save, setSave] = useState(false);
+  const [totalLike, setTotalLike] = useState(likes);
+  const updateLike = (isSave: boolean) => {
+      setSave(isSave)
+      setTotalLike((prev)=> isSave ? prev + 1 : prev - 1)
+  }
   return (
     <div>
       <figure className="relative group overflow-hidden rounded-md">
@@ -12,11 +21,20 @@ const ImageCard: FC<IImage> = ({ likes, name, shares, url }) => {
           height={500}
           className="w-full object-cover h-[250px] group-hover:scale-105 transition-all duration-200 group-hover:cursor-pointer"
         />
-        <figcaption className="absolute -bottom-20 w-full text-center bg-slate-800/50 text-white py-2 invisible group-hover:visible group-hover:bottom-0 transition-all duration-200 group-hover:cursor-pointer">
+        <figcaption className="absolute -bottom-20 w-full text-center bg-sky-400/50 text-white py-2 invisible group-hover:visible group-hover:bottom-0 transition-all duration-200 group-hover:cursor-pointer">
           <h3 className="font-bold text-sm tracking-wider">{name}</h3>
-          <p className="font-thin text-xs">
-            <span>{likes} likes</span> | <span>{shares} shares</span>
-          </p>
+          <div className="mx-5 flex justify-between items-center gap-5">
+            <p className="font-thin text-xs">
+              <span>{totalLike} likes</span> | <span>{shares} shares</span>
+            </p>
+            <Image
+              src={save ? saveDone : saveIcon}
+              onClick={() => updateLike(!save)}
+              alt=""
+              width={40}
+              height={40}
+            />
+          </div>
         </figcaption>
       </figure>
     </div>
